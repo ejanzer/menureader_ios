b04d86e70183b8cbaa86f1ac117fcb08686c575c
Emily Janzer <emily.janzer@gmail.com>
2014-03-18 11:41:40 -0700

    Initial Commit

---
+++ MenuReaderApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MenuReaderApp
+//
+//  Created by Emily Janzer on 3/18/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CMRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CMRAppDelegate class]));
+    }
+}
