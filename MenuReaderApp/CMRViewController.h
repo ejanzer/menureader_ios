45adf40d76c7f9539b4971b7b45d12a2a4c86c31
Emily Janzer <emily.janzer@gmail.com>
2014-03-18 15:20:26 -0700

    used UIImagePickerController to create a basic image picker app based on sample code

---
+++ MenuReaderApp/CMRViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMRViewController.h
+//  MenuReaderApp
+//
+//  Created by Emily Janzer on 3/18/14.
+//  Copyright (c) 2014 Emily Janzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMRViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+
+@end
---
+++ MenuReaderApp/CMRViewController.m
@@ -0,0 +1,132 @@
+//
+//  CMRViewController.m
+//  MenuReaderApp
+//
+//  Created by Emily Janzer on 3/18/14.
+//  Copyright (c) 2014 Emily Janzer. All rights reserved.
+//
+
+#import "CMRViewController.h"
+
+@interface CMRViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolBar;
+
+@property (nonatomic) UIImagePickerController *imagePickerController;
+@property (nonatomic) NSMutableArray *capturedImages;
+
+@end
+
+@implementation CMRViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.capturedImages = [[NSMutableArray alloc] init];
+    
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        // There is not a camera on this device, so don't show the camera button.
+        NSMutableArray *toolbarItems = [self.toolBar.items mutableCopy];
+        [toolbarItems removeObjectAtIndex:2];
+        [self.toolBar setItems:toolbarItems animated:NO];
+    }
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showImagePickerForCamera:(id)sender {
+    [self showImagePickerForSourceType:UIImagePickerControllerSourceTypeCamera];
+}
+
+- (IBAction)showImagePickerForPhotoPicker:(id)sender {
+    [self showImagePickerForSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+}
+
+
+- (void)showImagePickerForSourceType:(UIImagePickerControllerSourceType)sourceType {
+    
+    if (self.imageView.isAnimating) {
+        // Is this if it's a movie playing in the imageView?
+        [self.imageView stopAnimating];
+    }
+    
+    if (self.capturedImages.count > 0) {
+        // Get rid of any leftover captured images.
+        [self.capturedImages removeAllObjects];
+    }
+    
+    // TODO: Reset captured images to 0 (see tutorial)
+    
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    
+    // What does this line do?
+    imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
+    
+    // Set the source type for the imagePickerController as Photo Library or Camera.
+    imagePickerController.sourceType = sourceType;
+
+    // Sets self as the delegate.
+    imagePickerController.delegate = self;
+    
+    if (sourceType == UIImagePickerControllerSourceTypeCamera) {
+        // Once I have an overlay I can switch this to NO.
+        imagePickerController.showsCameraControls = YES;
+        
+        // TODO: Set up overlay view here. (see tutorial)
+    }
+    
+    // Set self's imagePickerController to the one we just created.
+    self.imagePickerController = imagePickerController;
+    // Display the imagePickerController.
+    [self presentViewController:self.imagePickerController animated:YES completion:nil];
+}
+
+- (void)finishAndUpdate {
+    // I might not need to use this if I'm not handling multiple images – I can probably just put this into didFinishPickingMediaWithInfo.
+    [self dismissViewControllerAnimated:YES completion:NULL]; // why null instead of nil?
+    if ([self.capturedImages count] > 0) {
+        if ([self.capturedImages count] == 1) {
+            // Camera took a single picture.
+            // TODO: Send it away for processing.
+            [self.imageView setImage:[self.capturedImages objectAtIndex:0]];
+        } else {
+            // Can use the list of images for animation. I don't think I'll be doing that.
+        }
+        
+        // Clear captured images to be ready to start again.
+        [self.capturedImages removeAllObjects];
+    }
+    self.imagePickerController = nil;
+}
+
+#pragma mark – UIImagePickerControllerDelegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    
+    [self.capturedImages addObject:image];
+    
+    [self finishAndUpdate];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+@end
