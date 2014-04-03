5855f732615a030722a46e3e91efcf26af740d55
Emily Janzer <emily.janzer@gmail.com>
2014-04-03 12:51:36 -0700

    moved server url to header file

---
+++ Server.h
@@ -0,0 +1,15 @@
+//
+//  Server.h
+//  Menu Reader
+//
+//  Created by Emily Janzer on 4/3/14.
+//  Copyright (c) 2014 Emily Janzer. All rights reserved.
+//
+
+#ifndef Menu_Reader_Server_h
+#define Menu_Reader_Server_h
+
+#define kBaseURL @"http://7558c64f.ngrok.com"
+
+#endif
+
